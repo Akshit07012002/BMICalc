80828c5b9777eb8ab3ed1a5f34a1b350b358cb7f
Akshit07012002 <74170127+Akshit07012002@users.noreply.github.com>
2021-11-17 15:15:00 +0530

    BMI CALCULATOR USING FLUTTER

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:bmi_calc/screen/home.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI CALCULATOR',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
